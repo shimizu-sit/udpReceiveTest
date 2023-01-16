56ad852eaac6bc758cff09a039fbfb0674f662eb
Shimizu Tetsuya <shimizu@info.shonan-it.ac.jp>
2023-01-16 15:43:25 +0900

    Create Unit Project

---
+++ Assets/UDPReceive.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+public class UDPReceive : MonoBehaviour
+{
+    int LOCAL_PORT = 22222;
+    static UdpClient udp;
+    Thread thread;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        udp = new UdpClient(LOCAL_PORT);
+        udp.Client.ReceiveTimeout = 10000;
+        thread = new Thread(new ThreadStart(ThreadMethod));
+        thread.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnApplicationQuit()
+    {
+        thread.Abort();
+    }
+
+    private static void ThreadMethod()
+    {
+        while(true)
+        {
+            IPEndPoint remoteEP = null;
+            byte[] data = udp.Receive(ref remoteEP);
+            //string text = Encoding.ASCII.GetString(data);
+            string text = Encoding.ASCII.get
+            Debug.Log(text);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # udpReceiveTest
  UnityでUDPを受信するプログラム
+
+## 参考サイト
+1. [UnityでUDPを受信してみる - Qiita](https://qiita.com/nenjiru/items/8fa8dfb27f55c0205651)
